babecb65ecdfbfd7e8c71b8fa18e673e8e15d9cb
Anas <anaswail246@gmail.com>
2024-07-21 11:28:27 +0300

    v.2

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ let submit = document.getElementById('submit');
 
 submit.innerHTML = 'Add WebSite';
 
+let isUpdating = false;
+let currentElement = null;
+
 // Function to add delete event listeners and update 
 function addDeleteEventListeners() {
     // delete 
@@ -21,18 +24,9 @@ function addDeleteEventListeners() {
             webTitle.value = parent.querySelector('#title').innerText;
             webLink.value = parent.querySelector('.visit a').href;
 
-            // Add an event listener to the submit button to update the values
-            submit.addEventListener("click", () => {
-                parent.querySelector('#title').innerText = webTitle.value;
-                parent.querySelector('.visit a').href = webLink.value;
-
-                // Save updated elements in local storage
-                localStorage.setItem('output', document.querySelector('.outputContent').innerHTML);
-
-                // Clear the form values
-                webTitle.value = '';
-                webLink.value = '';
-            }, { once: true });
+            isUpdating = true;
+            currentElement = parent;
+            submit.innerHTML = 'Update WebSite';
         });
     });
 }
@@ -47,70 +41,82 @@ if (localStorage.getItem("output")) {
 
 // add Event onclick
 submit.addEventListener("click", () => {
-    if(webTitle.value != '' || webLink.value != "") {
-    submit.innerHTML = 'Add WebSite';
+    if (webTitle.value !== '' && webLink.value !== "") {
+        if (isUpdating) {
+            currentElement.querySelector('#title').innerText = webTitle.value;
+            currentElement.querySelector('.visit a').href = webLink.value;
 
-        console.log("Button clicked");
-        console.log("webTitle value:", webTitle.value);
-        console.log("webLink value:", webLink.value);
-    
-        // add output ul
-        let output = document.createElement('ul');
-        output.className = 'output output list-none text-white font-bold text-[15px] drop-shadow-md flex translate-x-[5%] md:translate-x-0 md:w-full justify-between bg-[#373737] rounded-[10px] mt-2 w-[90%]';
-    
-        // add output li
-        let li = [];
-        for (let i = 0; i < 4; i++) {
-            li[i] = document.createElement('li');
-    
-            // title
-            if (i == 0) {
-                li[i].innerHTML = webTitle.value;
-                li[i].id = "title";
+            // Save updated elements in local storage
+            localStorage.setItem('output', document.querySelector('.outputContent').innerHTML);
+
+            // Reset form and state
+            webTitle.value = '';
+            webLink.value = '';
+            submit.innerHTML = 'Add WebSite';
+            isUpdating = false;
+            currentElement = null;
+        } else {
+            console.log("Button clicked");
+            console.log("webTitle value:", webTitle.value);
+            console.log("webLink value:", webLink.value);
+
+            // add output ul
+            let output = document.createElement('ul');
+            output.className = 'output output list-none text-white font-bold text-[15px] drop-shadow-md flex translate-x-[5%] md:translate-x-0 md:w-full justify-between bg-[#373737] rounded-[10px] mt-2 w-[90%]';
+
+            // add output li
+            let li = [];
+            for (let i = 0; i < 4; i++) {
+                li[i] = document.createElement('li');
+
+                // title
+                if (i == 0) {
+                    li[i].innerHTML = webTitle.value;
+                    li[i].id = "title";
+                }
+
+                // visit link
+                else if (i == 1) {
+                    let link = document.createElement('a');
+                    link.innerText = "Visit";
+                    link.target = "_blank"; // Opens link in a new tab
+                    li[i].appendChild(link);
+                    li[i].className = "visit";
+                    link.href = webLink.value;
+
+                    console.log("link.href set to:", link.href);
+                }
+
+                // update btn
+                else if (i == 2) {
+                    li[i].innerText = 'Update';
+                    li[i].className = 'update';
+                }
+
+                // delete btn
+                else {
+                    li[i].innerText = 'Delete';
+                    li[i].className = 'delete';
+                }
+
+                // add lis to ul
+                output.appendChild(li[i]);
             }
-    
-            // visit link
-            else if (i == 1) {
-                let link = document.createElement('a');
-                link.innerText = "Visit";
-                link.target = "_blank"; // Opens link in a new tab
-                li[i].appendChild(link);
-                li[i].className = "visit";
-                link.href = webLink.value;
-    
-                console.log("link.href set to:", link.href);
-            }
-    
-            // update btn
-            else if (i == 2) {
-                li[i].innerText = 'Update';
-                li[i].className = 'update';
-            }
-    
-            // delete btn
-            else {
-                li[i].innerText = 'Delete';
-                li[i].className = 'delete';
-            }
-    
-            // add lis to ul
-            output.appendChild(li[i]);
+
+            // read elements in html
+            document.querySelector('.outputContent').appendChild(output);
+
+            // delete value from the forms
+            webTitle.value = '';
+            webLink.value = '';
+
+            // save elements in local storage
+            localStorage.setItem('output', document.querySelector('.outputContent').innerHTML);
+
+            // add event listeners for the delete buttons
+            addDeleteEventListeners();
         }
-    
-        // read elements in html
-        document.querySelector('.outputContent').appendChild(output);
-    
-        // delete value from the forms
-        webTitle.value = '';
-        webLink.value = '';
-    
-        // save elements in local storage
-        localStorage.setItem('output', document.querySelector('.outputContent').innerHTML);
-    
-        // add event listeners for the delete buttons
-        addDeleteEventListeners();
-    }
-    else{
-        submit.innerText = "Please enter the data correctly"
+    } else {
+        submit.innerText = "Please enter the data correctly";
     }
 });
